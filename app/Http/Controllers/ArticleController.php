51aac75be830a548ff5e8cdef15e1f4c57bfc2f5
Ivan Nikolovski <ivan.85bt@gmail.com>
2018-09-07 12:20:20 +0200

    initial commit

---
+++ app/Http/Controllers/ArticleController.php
@@ -0,0 +1,84 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use App\Article;
+use App\Http\Resources\Article as ArticleResource;
+use Illuminate\Http\Request;
+
+class ArticleController extends Controller
+{
+    /**
+     * Display a listing of the resource.
+     *
+     * @return \Illuminate\Http\Response
+     */
+    public function index()
+    {
+        //Get Articles
+        $articles = Article::paginate(15);
+        //Return a collection of articles as resource
+        return ArticleResource::collection($articles);
+    }
+
+    /**
+     * Show the form for creating a new resource.
+     *
+     * @return \Illuminate\Http\Response
+     */
+    public function create()
+    {
+        //
+    }
+
+    /**
+     * Store a newly created resource in storage.
+     *
+     * @param  \Illuminate\Http\Request  $request
+     * @return \Illuminate\Http\Response
+     */
+    public function store(Request $request)
+    {
+        $article = $request->isMethod('put')
+            ? Article::findOrFail($request->article_id)
+            : new Article;
+
+        $article->id = $request->input('article_id');
+        $article->title = $request->input('title');
+        $article->body = $request->input('body');
+
+        if ($article->save()) {
+            return new ArticleResource($article);
+        }
+    }
+
+    /**
+     * Display the specified resource.
+     *
+     * @param  int  $id
+     * @return \Illuminate\Http\Response
+     */
+    public function show($article)
+    {
+        //Get Article
+        $articles = Article::findOrFail($article);
+        //Return a single article as resource
+        return new ArticleResource($articles);
+    }
+
+    /**
+     * Remove the specified resource from storage.
+     *
+     * @param  int  $id
+     * @return \Illuminate\Http\Response
+     */
+    public function destroy($article)
+    {
+        //Get Article
+        $articles = Article::findOrFail($article);
+
+        if ($articles->delete()) {
+            return new ArticleResource($articles);
+        }
+    }
+}
